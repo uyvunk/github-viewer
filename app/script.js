d2aa257a4798cf65f3530f889fb3e8371c0ac25b
uyvunk <uyvunk@gmail.com>
2017-12-12 06:57:25 -0800

    WIP

--- app/script.js
+++ app/script.js
@@ -1,26 +1,31 @@
 (function(){
-  var MainController = function($scope, $http) {
+
+  var app = angular.module("githubViewer", []);
+
+  var MainController = function($scope, github, $location, $anchorScroll) {
     $scope.pageTitle = "Github Profile Viewer";
     $scope.filterType = "+name";
 
-    var onUserComplete = function(response) {
-      $scope.user = response.data;
-      $http.get($scope.user.repos_url)
-           .then(onRepos, onError);
+    var onUserComplete = function(data) {
+      $scope.user = data;
+      github.getRepos(data)
+            .then(onRepos, onError);
     }
 
     var onError = function(reason) {
       $scope.error = "Could not fetch the data";
     };
 
-    var onRepos = function(response) {
-      $scope.repos = response.data;
+    var onRepos = function(data) {
+      $scope.repos = data;
+      $location.hash("userName");
+      $anchorScroll();
     };
 
     $scope.search = function(username) {
       if (username) {
-        $http.get("https://api.github.com/users/" + username)
-          .then(onUserComplete, onError);
+        github.getUser(username)
+              .then(onUserComplete, onError);
       }
     };
   };
